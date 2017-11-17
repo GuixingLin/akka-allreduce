19096b44d80289520ca8d45a57eeb63b518e6e4e
Lincoln Guixing Lin <lincolnglin@Guixings-MacBook-Pro.local>
2017-11-16 23:05:43 -0800

    still buggy

--- src/main/scala/sample/cluster/allreduce/AllreduceWorker.scala
+++ src/main/scala/sample/cluster/allreduce/AllreduceWorker.scala
@@ -16,12 +16,13 @@ class AllreduceWorker extends Actor {
   var peers = Map[Int, ActorRef]()  // workers in the same row/col, including self
   var thReduce = 1.0    // pct of scattered data needed to start reducing 
   var thComplete = 1.0  // pct of reduced data needed to complete current round
-  var maxLag = 0    // number of rounds allowed for a worker to fall behind
-  var data : Array[Double] = Array.empty  // input buffer
-  var scatterBuf : Array[Double] = Array.empty  // buffer to store scattered data received
-  var reduceBuf : Array[Double] = Array.empty   // buffer to store reduced data received
-  var aggregatedData  : Double = 0  // obtained by aggregating scatterBuf
-  var round = -1 // current round of allreduce
+  var maxLag = 0        // number of rounds allowed for a worker to fall behind
+  var round = -1        // current round of allreduce
+  var maxRound = -1 // most updated timestamp received for StartAllreduce
+  var data : Array[Double] = Array.empty            // store input data
+  var scatterBuf : Array[Array[Double]] = Array.empty  // store scattered data received
+  var reduceBuf : Array[Array[Double]] = Array.empty   // store reduced data received
+  var aggregatedData : Double = 0  // result of aggregating scatterBuf
 
   def receive = {
 
@@ -32,17 +33,28 @@ class AllreduceWorker extends Actor {
       thReduce = init.thReduce
       thComplete = init.thComplete
       maxLag = init.maxLag
-      round = -1 // clear round info to start over
+      round = -1    // clear round info to start over
+      maxRound = -1
+      scatterBuf = new Array[Array[Double]](maxLag + 1)
+      reduceBuf = new Array[Array[Double]](maxLag + 1)
+      for (row <- 0 to maxLag) {
+        scatterBuf(row) = new Array[Double](peers.size + 1) // extra space for tracking number of elems
+        reduceBuf(row) = new Array[Double](peers.size + 1)
+      }
       println(s"----id = ${id}")
       for (i <- 0 until peers.size) {
         println(s"----peers[${i}] = ${peers(i)}")
       }
       println(s"----number of peers = ${peers.size}")
+      println(s"----thReduce = ${thReduce}, thComplete = ${thComplete}, maxLag = ${maxLag}")
+      println(s"----size of buffer: ${scatterBuf.size} x ${scatterBuf(0).size}")
 
     case start : StartAllreduce =>
       println(s"----start allreduce round ${start.round}")
       if (start.round > round) {
         round = start.round
+        bufferUp(scatterBuf)
+        bufferUp(reduceBuf)
         initData(round)
         scatter()
       }
@@ -50,9 +62,10 @@ class AllreduceWorker extends Actor {
     case s : Scatter =>
       println(s"----receive scattered data from round ${s.round}: value = ${s.value}, srcId = ${s.srcId}, destId = ${s.destId}")
       assert(s.destId == id || id == -1)
-      storeScatteredData(s.value, s.srcId) //? necessary?
-      if (scatterBuf.length >= peers.size * thReduce && id != -1) {
-        println(s"----receive ${scatterBuf.length} scattered data (numPeers = ${peers.size}) for round ${round}, start reducing")
+      val row = s.round - round //??? what if round == -1?
+      storeScatteredData(s.value, s.srcId, row)
+      if (scatterBuf(row)(peers.size) >= peers.size * thReduce && id != -1) {
+        println(s"----receive ${scatterBuf(row)(peers.size)} scattered data (numPeers = ${peers.size}) for round ${round}, start reducing")
         aggregatedData = aggregate()
         broadcast(aggregatedData)
       }
@@ -60,11 +73,15 @@ class AllreduceWorker extends Actor {
     case r : Reduce =>
       println(s"----receive reduced data from round ${r.round}: value = ${r.value}, srcId = ${r.srcId}, destId = ${r.destId}")
       assert(r.destId == id || id == -1)
-      storeReducedData(r.value, r.srcId)
-      if (reduceBuf.length >= peers.size * thComplete && id != -1) {
-        println(s"----receive ${reduceBuf.length} reduced data (numPeers = ${peers.size}) for round ${round}, complete")
+      val row = r.round - round
+      storeReducedData(r.value, r.srcId, row)
+      if (reduceBuf(row)(peers.size) >= peers.size * thComplete && id != -1) {
+        println(s"----receive ${reduceBuf(row)(peers.size)} reduced data (numPeers = ${peers.size}) for round ${round}, complete")
         update()
         complete(round)
+        if (round < maxRound) {
+          self ! StartAllreduce(round + 1)
+        }
       }
 
     case Terminated(a) =>
@@ -73,6 +90,13 @@ class AllreduceWorker extends Actor {
           peers -= idx
         }
       }
+  }
+
+  private def bufferUp(buf : Array[Array[Double]]) = {
+    for (i <- 1 until buf.size) {
+      buf(i-1) = buf(i)
+    }
+    buf(buf.size - 1) = new Array[Double](peers.size + 1)
   }
 
   private def initData(round : Int) = {
@@ -96,19 +120,21 @@ class AllreduceWorker extends Actor {
     }
   }
 
-  private def storeScatteredData(data : Double, srcId : Int) = {
-    scatterBuf :+= data
+  private def storeScatteredData(data : Double, srcId : Int, row : Int) = {
+    scatterBuf(row)(srcId) = data
+    scatterBuf(row)(peers.size) += 1
   }
 
-  private def storeReducedData(data: Double, srcId : Int) = {
-    reduceBuf :+= data
+  private def storeReducedData(data: Double, srcId : Int, row : Int) = {
+    reduceBuf(row)(srcId) = data
+    reduceBuf(row)(peers.size) += 1
   }
 
   private def aggregate() : Double = {
     println(s"----start aggregating")
     aggregatedData = 0
-    for (v <- scatterBuf) {
-      aggregatedData += v
+    for (col <- 0 until peers.size) {
+      aggregatedData += scatterBuf(0)(col)
     }
     return aggregatedData
   }
@@ -119,8 +145,6 @@ class AllreduceWorker extends Actor {
 
   private def complete(round : Int) = {
     println(s"----complete allreduce round ${round}\n")
-    scatterBuf = Array.empty
-    reduceBuf = Array.empty 
     data = Array.empty
     master.orNull ! CompleteAllreduce(id, round)
   }
